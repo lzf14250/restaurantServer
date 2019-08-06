6aa740e5bbe2860b723b290d66e1e8693e5e9819
DESKTOP-JGS4QIP\lzf95 <zhuofu.li@wustl.edu>
2019-08-06 13:19:47 -0500

    Express Session Part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -30,10 +32,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 function auth(req, res, next) {
-  console.log(req.signedCookies);
+  console.log(req.session);
 
-  if (!req.signedCookies.user) {
-    // if the cookie doesn't exist
+  if (!req.session.user) {
+    // if the session username doesn't exist
 
     var authHeader = req.headers.authorization;
 
@@ -49,7 +51,7 @@ function auth(req, res, next) {
       var pass = auth[1];
 
     if (user === 'admin' && pass === 'password') {
-      res.cookie('user','admin', { signed: true });
+      req.session.user = 'admin';
       next();
     } else {
       var err = new Error('You are not authorized: Wrong Username or Password!');
@@ -58,8 +60,8 @@ function auth(req, res, next) {
       return next(err);
     }
   } else {
-    // cookie exists
-    if (req.signedCookies.user === 'admin') {
+    // session exists
+    if (req.session.user === 'admin') {
       next();
     } else {
       var err = new Error('Fake Signed Cookies');
@@ -73,7 +75,15 @@ function auth(req, res, next) {
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 app.use(auth);
 
